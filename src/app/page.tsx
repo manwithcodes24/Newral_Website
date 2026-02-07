5bd09d1f7ff080f546a5bfac2461c9bc13d3b406
Your Name <bhanupss137@gmail.com>
2026-02-07 11:32:31 +0530

    new new sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 // app/page.tsx
 import BlogSection from "@/components/landingpage/BlogSection";
-import CardHeroSection from "@/components/landingpage/TeglineSection";
+import CardHeroSection from "@/components/landingpage/TaglineSection";
 import HeroSection from "@/components/landingpage/HeroSection";
 import ReviewSection from "@/components/landingpage/Reviews";
 import ReviewSection2 from "@/components/landingpage/CompetishunReviewSection";
 import ServicesSection from "@/components/landingpage/ServicesSection";
-import FAQSection from "@/components/landingpage/wegotanswered";
+import FAQSection from "@/components/landingpage/WeGotAnswered";
 
 export default function Home() {
   return (
