ada5b989d9a918dd19dab71bb94d12fa11d5608b
Your Name <bhanupss137@gmail.com>
2026-02-07 15:38:05 +0530

    fix build :

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { ThemeToggle } from "@/components/theme-toggle";
-import Navbar from "@/components/header";
+import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
 import localFont from 'next/font/local';
 import { Analytics } from "@vercel/analytics/next"
