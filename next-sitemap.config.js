1f5474457b4a40cd8511f3ac58134f230379aa40
Your Name <bhanupss137@gmail.com>
2026-04-24 22:25:09 +0530

    newnew new

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,6 +2,12 @@
 module.exports = {
   siteUrl: 'https://newral.in',
   generateRobotsTxt: true,
+  exclude: [
+    '/admin',
+    '/admin/*',
+    '/api/*',
+    '/_next/*'
+  ],
   changefreq: 'weekly',
   priority: 0.7,
   sitemapSize: 7000,
